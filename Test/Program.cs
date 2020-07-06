d1d7f1b872eff8ce7a9e9e7e366f471f11da72f6
egorkrivoshapka <egorkrivoshapka@gmail.com>
2020-07-06 11:55:18 +0300

    Small refactor

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,13 +12,14 @@ namespace Test
 
         public static void Main(string[] args)
         {
+
+            //Example 1
             int a = 5;
             Modify(a);
             Console.WriteLine(a);
 
 
-
-
+            //Example 2
             var s = new SomeClass()
             {
                 num = a
@@ -26,7 +27,19 @@ namespace Test
             Modify(s);
             Console.WriteLine($"SomeClass: {s.num}");
 
-            List<SomeClass> list = new List<SomeClass>()
+
+            //Example 3
+            List<int> originalListInt = new List<int>()
+            {
+                1,2,3,4,5,6
+            };
+
+            var value = originalListInt[3];
+            value = 25;
+            Console.WriteLine(originalListInt[3]);
+
+
+            List<SomeClass> originalObjectList = new List<SomeClass>()
             {
                 new SomeClass()
                 {
@@ -50,29 +63,24 @@ namespace Test
                 }
             };
 
-
-            var temp = list[3];
+            var temp = originalObjectList[3];
             temp.num = 20;
-            Console.WriteLine(list[3].num);
+            Console.WriteLine(originalObjectList[3].num);
 
-            List<int> listInt = new List<int>()
+            //Range, Creates a shallow copy?
+            var listRangeObjects = originalObjectList.GetRange(3, 2);
+            for (int i = 0; i < listRangeObjects.Count; i++)
             {
-                1,2,3,4,5,6
-            };
-
-            var value = listInt[3];
-            value = 25;
-            Console.WriteLine(listInt[3]);
-
-            var listRange = list.GetRange(3, 2);
-            for(int i = 0; i<listRange.Count; i++)
-            {
-                listRange[i] = null;
+                listRangeObjects[i].num = 2000;
+                //listRange[i] = null;
             }
-            foreach(var item in list)
+            Console.WriteLine("Original list objects");
+            foreach (var item in originalObjectList)
             {
-                Console.WriteLine(item.num);
+                Console.Write($"{item.num},");
             }
+
+
         }
 
         public static int Modify(int a)
@@ -80,11 +88,6 @@ namespace Test
             a++;
             Console.WriteLine(a);
             return a;
-        }
-
-        public static int Modify(int a, int b)
-        {
-            return 0;
         }
 
         public static void Modify(SomeClass a)
